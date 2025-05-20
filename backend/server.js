cebf2f06cf27001dee23779366b7031ba0389da1
levisamuell <demolitiongaming2005@gmail.com>
2025-05-20 05:41:43 +0530

    added a get route for testing

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,13 @@ app.use(express.json());
 app.use('/user', UserRoute);
 
 mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log("Successfully connected to the DB!!"))
-    .catch((err) => console.log("Error connecting to the DB", err))
+.connect(process.env.MONGO_URI)
+.then(() => console.log("Successfully connected to the DB!!"))
+.catch((err) => console.log("Error connecting to the DB", err))
 
+app.get('/', (req, res) => {
+  res.send("Welcome to GrubHub Backend API ");
+});
 app.listen(PORT, () => {
     console.log(`App is listening at ${PORT}`)
 })
